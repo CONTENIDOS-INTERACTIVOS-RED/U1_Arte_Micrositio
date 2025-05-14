48c1a6538fa59b9921329455ddf2b2310a031caa
Francisco Lizcano <flizcanor@icloud.com>
2025-05-13 23:08:17 -0500

    Update Tema4.vue

--- src/views/curso/Tema4.vue
+++ src/views/curso/Tema4.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .curso-main-container.pb-3
-    BannerInterno(:subTitulo="'4. Eventos culturales-artísticos online'")
+    BannerInterno(:subTitulo="'4. Eventos culturales-artísticos <em>online</em>'")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
 
       .row.mb-5
